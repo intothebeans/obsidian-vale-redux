6643971dc872575797d247bd4c994457989d4e23
intothebeans <88350376+intothebeans@users.noreply.github.com>
2026-02-11 20:15:38 -0500

    refactor: start from scratch

--- src/context.ts
+++
@@ -1,7 +0,0 @@
-import { App } from "obsidian";
-import * as React from "react";
-import { ValeSettings } from "./types";
-
-export const AppContext = React.createContext<App | undefined>(undefined);
-
-export const SettingsContext = React.createContext<ValeSettings | undefined>(undefined);
