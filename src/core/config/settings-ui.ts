469ca840809d2a7381ff4142b9e917f815b8d247
intothebeans <88350376+intothebeans@users.noreply.github.com>
2026-02-16 05:31:10 +0000

    feat(settings): display global block settings
    Signed-off-by: intothebeans <88350376+intothebeans@users.noreply.github.com>

--- src/core/config/settings-ui.ts
+++ src/core/config/settings-ui.ts
@@ -9,6 +9,7 @@ export function createConfigUI(
 	plugin: ValePlugin,
 ): void {
 	createCoreSettings(container, plugin);
+	createGlobalSettings(container, plugin);
 }
 
 function createCoreSettings(
@@ -18,7 +19,7 @@ function createCoreSettings(
 	// TODO: read installed packages from config file
 	const config: ValeConfig = plugin.valeConfig;
 	return new SettingGroup(container)
-		.setHeading("Core Settins")
+		.setHeading("Core Settings")
 		.addClass("vale-config-core-settings")
 		.addSearch((search) => {
 			search.setPlaceholder("Search settings...");
@@ -83,6 +84,73 @@ function createCoreSettings(
 				"Ignored classes",
 				"Classes to ignore for both inline and block-level tags.",
 			);
+		});
+}
+
+function createGlobalSettings(
+	container: HTMLElement,
+	plugin: ValePlugin,
+): SettingGroup {
+	const config: ValeConfig = plugin.valeConfig;
+	const globalConfig = config["*"];
+	return new SettingGroup(container)
+		.setHeading("Global Settings")
+		.addClass("vale-config-global-settings")
+		.addSetting((setting) => {
+			stringArraySetting(
+				setting,
+				globalConfig?.BasedOnStyles,
+				"Based on styles",
+				"List of styles to apply globally.",
+			);
+		})
+		.addSetting((setting) => {
+			stringArraySetting(
+				setting,
+				globalConfig?.BlockIgnores,
+				"Block ignores",
+				"List of block-level HTML tags to ignore globally.",
+			);
+		})
+		.addSetting((setting) => {
+			stringArraySetting(
+				setting,
+				globalConfig?.TokenIgnores,
+				"Token ignores",
+				"List of inline-level HTML tags to ignore globally.",
+			);
+		})
+		.addSetting((setting) => {
+			setting
+				.setName("Language")
+				.setDesc("Language to use for global checks.")
+				.addText((text) => {
+					text.setValue(globalConfig?.Lang || "").setPlaceholder(
+						"Language code (e.g., en, fr, etc.)",
+					);
+				});
+		})
+		.addSetting((setting) => {
+			setting
+				.setName("Check overrides")
+				.setDesc(
+					"Override specific checks globally. Specify the check name, and optionally set a new alert level or disable it.",
+				)
+				.addTextArea((text) => {
+					const overrides = globalConfig?.CheckOverrides;
+					const overrideLines = [];
+					if (overrides && overrides.length > 0) {
+						for (const override of overrides) {
+							overrideLines.push(
+								`${override.Check} = ${override?.Enabled === false ? "NO" : override?.Level || "no change"}`,
+							);
+						}
+					}
+					text.setValue(overrideLines.join("\n")).setPlaceholder(
+						// eslint-disable-next-line obsidianmd/ui/sentence-case
+						"One override per line, format: CheckName = Level or NO (to disable)",
+					);
+				});
 		});
 }
 
