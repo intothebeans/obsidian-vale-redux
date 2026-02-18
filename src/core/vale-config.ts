c1164ffa05be6c86f339d2e934fd957a78f22e5a
intothebeans <88350376+intothebeans@users.noreply.github.com>
2026-02-17 22:08:56 -0500

    feat(config): add function to rotate backups

--- src/core/vale-config.ts
+++ src/core/vale-config.ts
@@ -53,6 +53,33 @@ export async function backupExistingConfig(plugin: ValePlugin): Promise<void> {
 	await copyFile(configPath, outputPath);
 }
 
+export async function rotateBackups(plugin: ValePlugin): Promise<void> {
+	const maxBackups = plugin.settings.valeConfigBackupsToKeep;
+	const backups = plugin.settings.backupPaths;
+	if (backups.length <= maxBackups) {
+		return;
+	}
+
+	const parsed = backups.map((p) => {
+		// name, "backup", ts.ini
+		const name = path.parse(p).name;
+		const tokens = name.split("_");
+		const tsToken = tokens.find((t) => t.endsWith(".ini"));
+		const ts = tsToken ? tsToken.replace(".ini", "") : "";
+		return { path: p, ts };
+	});
+
+	parsed.sort((a, b) => b.ts.localeCompare(a.ts)); // Newest first
+
+	const toDelete = parsed.slice(maxBackups);
+	for (const backup of toDelete) {
+		const file = plugin.app.vault.getFileByPath(backup.path);
+		if (file) {
+			await plugin.app.fileManager.trashFile(file);
+		}
+	}
+}
+
 /** @throws file system errors */
 export async function writeConfigToFile(
 	configPath: string,
